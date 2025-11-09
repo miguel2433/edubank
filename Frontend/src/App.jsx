17370026e7537692cc04ed7ccae007d00eea4117
miguel2433 <miguelitocap12@gmail.com>
2025-11-09 20:28:08 -0300

    funciona

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 export default function App() {
   return (
@@ -10,7 +11,14 @@ export default function App() {
       <Route path="/login" element={<Login />} />
 
       {/* Rutas protegidas */}
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { authService } from "../services/authService";
 
 export default function Dashboard() {
-
+  const handleLogout = async () => {
+    await authService.logout();
+    window.location.href = "/login";
+  };
 
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-4">Panel principal</h1>
       <button
-        // onClick={}
+        onClick={handleLogout}
         className="bg-red-600 text-white px-4 py-2 rounded-lg"
       >
         Cerrar sesión
