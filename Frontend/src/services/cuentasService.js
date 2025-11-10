821ee7a58fe4b28ee09e271bf859ce7ef27a7be1
miguel2433 <miguelitocap12@gmail.com>
2025-11-09 22:29:56 -0300

    crearcuenta service

--- Frontend/src/services/cuentasService.js
+++ Frontend/src/services/cuentasService.js
@@ -14,4 +14,21 @@ export const cuentasService = {
 
     return res.json();
   },
+    async crearCuenta(data) {
+    const res = await fetch(`${API_URL}`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        throw new Error(error.message || "Error al crear la cuenta del usuario");
+    }
+
+    return res.json();
+    }
 };
